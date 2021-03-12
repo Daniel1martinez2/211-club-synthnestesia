27f6d4978c281b2746c1062cebfdf645354445ba
augustoramirez46 <tutoead46@gmail.com>
2021-03-11 19:54:11 -0500

    Co-authored-by: Daniel Martinez villegas <Daniel1martinez2@users.noreply.github.com> Co-authored-by: Sebastián Gaviria <vagyriag@users.noreply.github.com>

--- public/js/audioTest.js
+++ public/js/audioTest.js
@@ -1,111 +1,110 @@
-var drums = null;
+const btn = document.createElement('button');
+btn.innerText = 'Begin';
+document.body.appendChild(btn);
 
-window.AudioContext = window.AudioContext ||
-window.webkitAudioContext;
+btn.addEventListener('click', () => {
+  const { analyserNode, dataArray, bufferLength } = getAudioInfo();
+  const { canvasCtx, canvas } = getCanvasInfo();
 
-var context = new AudioContext();;
+  let barSelected = 0;
+  let threshold = 0;
+  let lastTouch = 0;
 
-function loadDrumSound(url){
+  window.addEventListener('keydown', ({ key }) => {
+    switch(key) {
+      case 'ArrowRight':
+        barSelected += 1;
+        if(barSelected >= bufferLength) barSelected = 0;
+        break;
+      case 'ArrowLeft':
+        barSelected -= 1;
+        if(barSelected < 0) barSelected = bufferLength - 1;
+        break;
+      case 'ArrowUp':
+        threshold += 1;
+        break;
+      case 'ArrowDown':
+        threshold -= 1;
+        break;
+    }
+  });
 
-    fetch("/test-audio-files/drums.wav").then((res) => {
-        return res.arrayBuffer();
-    }).then((res) => {
-        
-        context.decodeAudioData(res, function(buffer) {
-            drums = buffer;
+  function draw() {
+    //Schedule next redraw
+    requestAnimationFrame(draw);
+  
+    //Get spectrum data
+    analyserNode.getFloatFrequencyData(dataArray);
+  
+    //Draw black background
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    //Draw spectrum
+    const barWidth = (canvas.width / bufferLength);
 
-            var source = context.createBufferSource(); // creates a sound source
-            source.buffer = buffer;                    // tell the source which sound to play
-            source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-            //console.log(context.sampleRate);
-            const analyser = context.createAnalyser();
-            source.connect(analyser);
-            analyser.fftSize = 2048;
-            var bufferLength = analyser.frequencyBinCount;
-            var dataArray = new Float32Array(bufferLength);
-            analyser.getByteTimeDomainData(dataArray);
-            console.log(dataArray)
-            //for (var i = 0; i < bufferLength; i++) {}
-         }, console.log);
-    });
-    
-    
+    const getHeight = (n) => (n + 90) * 6;
+
+    for (let i = 0; i < bufferLength; i++) {
+      const barHeight = getHeight(dataArray[i]);
+      canvasCtx.fillStyle = i === barSelected ? 'rgb(255,200, 0)' : 'rgb(220, 50, 50)';
+      canvasCtx.fillRect(barWidth * i, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+    }
+
+    canvasCtx.fillStyle = 'rgb(40, 250, 30)';
+    canvasCtx.fillRect(barSelected * barWidth, canvas.height - getHeight(threshold) / 2, barWidth, 2);
+
+    if(dataArray[barSelected] >= threshold && Date.now() - lastTouch > 200) {
+      lastTouch = Date.now();
+      console.log('touch', dataArray[barSelected]);
+    }
+  };
+
+  draw();
+});
+
+
+const getCanvasInfo = () => {
+  //Create 2D canvas
+  const canvas = document.createElement('canvas');
+  canvas.style.position = 'absolute';
+  canvas.style.top = 0;
+  canvas.style.left = 0;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  document.body.appendChild(canvas);
+  const canvasCtx = canvas.getContext('2d');
+  canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+  return {
+    canvasCtx,
+    canvas,
+  }
 }
 
-window.addEventListener('load', loadDrumSound, false);
 
-function getPeaksAtThreshold(data, threshold) {
-    var peaksArray = [];
-    var length = data.length;
-    
-    for(var i = 0; i < length;) {
-      if (data[i] > threshold) {
-        peaksArray.push(i);
-        // Skip forward ~ 1/4s to get past this peak.
-        i += 10000;
-      }
-      i++;
-    }
-    return peaksArray;
-  }
-
+const getAudioInfo = () => {
   const audioCtx = new AudioContext();
+  //Create audio source
+  //Here, we use an audio file, but this could also be e.g. microphone input
+  const audioEle = new Audio();
+  audioEle.src = "/test-audio-files/drums.wav";//insert file name here
+  audioEle.autoplay = true;
+  audioEle.loop = true;
+  audioEle.preload = 'auto';
+  const audioSourceNode = audioCtx.createMediaElementSource(audioEle);
+  //Create analyser node
+  const analyserNode = audioCtx.createAnalyser();
+  analyserNode.fftSize = 32;
+  const bufferLength = analyserNode.frequencyBinCount;
+  const dataArray = new Float32Array(bufferLength);
+  //Set up audio node network
+  audioSourceNode.connect(analyserNode);
+  analyserNode.connect(audioCtx.destination);
 
-//Create audio source
-//Here, we use an audio file, but this could also be e.g. microphone input
-const audioEle = new Audio();
-audioEle.src = "/test-audio-files/drums.wav";//insert file name here
-audioEle.autoplay = false;
-audioEle.preload = 'auto';
-const audioSourceNode = audioCtx.createMediaElementSource(audioEle);
-
-//Create analyser node
-const analyserNode = audioCtx.createAnalyser();
-analyserNode.fftSize = 256;
-const bufferLength = analyserNode.frequencyBinCount;
-const dataArray = new Float32Array(bufferLength);
-
-//Set up audio node network
-audioSourceNode.connect(analyserNode);
-analyserNode.connect(audioCtx.destination);
-
-//Create 2D canvas
-const canvas = document.createElement('canvas');
-canvas.style.position = 'absolute';
-canvas.style.top = 0;
-canvas.style.left = 0;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-document.body.appendChild(canvas);
-const canvasCtx = canvas.getContext('2d');
-canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-
-function draw() {
-  //Schedule next redraw
-  requestAnimationFrame(draw);
-
-  //Get spectrum data
-  analyserNode.getFloatFrequencyData(dataArray);
-
-  //Draw black background
-  canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-  //Draw spectrum
-  const barWidth = (canvas.width / bufferLength) * 2.5;
-  
-  let sum = 0;
-  for (let i = 0; i < bufferLength; i++) {
-    sum += dataArray[i];
-  }
-  const avg = sum/dataArray.length;
-
-  if(avg > -70) console.log(avg);
-  /* let height = 0;
-  const barHeight = (dataArray[i] + 140) * 6;
-    canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight + 100) + ', 50, 50)';
-    canvasCtx.fillRect(posX, canvas.height - barHeight / 2, barWidth, barHeight / 2);
-    posX += barWidth + 1; */
-};
-
-draw();
+  return {
+    analyserNode,
+    dataArray,
+    bufferLength,
+  };
+}
